ac20802975aff8c291e35712d358449c8df4cafe
shengyu.yang <shengyu.yang@chinacache.com>
2013-10-30 18:19:46 +0800

    add jquery add css

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,3 +3,11 @@
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+// 	alert(window.jQuery)
+// });
+// chrome.runtime.onStartup.addListener(function (details) {
+// 	console.log("tool Startup!");
+// 	alert(1)
+// });
