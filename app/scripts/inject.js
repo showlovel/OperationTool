d58d28596a8f392f1e0e41049ec0cfa68ab5f157
shengyu.yang <shengyu.yang@chinacache.com>
2013-11-01 17:57:01 +0800

    add infodiv toggle

--- app/scripts/inject.js
+++ app/scripts/inject.js
@@ -1,5 +1,5 @@
-function createMenus(){
-	div       = document.createElement("div")
-	div.class = "spdiv"
-	div.id    = "spmenus"
+function injectInfoDiv(){
+	div = "<div id='infoDiv' class='infoDiv'><div class='r'>OperationTool v0.1  <a href='' id='infoClose'>close</a></div><div id='actinfo' class='infodate'></div></div>"
+	$("body").append(div)
 }
+injectInfoDiv()
--- app/scripts/process.js
+++ app/scripts/process.js
@@ -17,34 +17,66 @@ document.onmouseup=function(env){
     data = text.toString()
     if(data.length!=0 && data.length!=""){
         console.log(text.toString())
-        getMouse(env)
-        //check wether ip
-        if(/\d+.\d+.\d+.\d+/.test(data)){
+        place = getMouse(env)
+        //check ip
+        var info = ""
+        if(/^\d+\.\d+\.\d+\.\d+$/.test(data)){
             console.log("is a ip")
-            checkIp(data)
-        }
-        if(/\w+-\w+-\w+-\w+/.test(data)){
+            info = checkIp(data,place)
+        }else if(/^[A-Za-z]{3}-[A-Za-z]{2,3}-[A-Za-z0-9]{1}-[A-Za-z0-9]{3}$/.test(data)){
+        //check device
             console.log("is a device")
-            checkDevice(data)
+            info = checkDevice(data)
+        }else if(/^[A-Za-z0-9.-]+$/.test(data)){
+        //check group
+            console.log("is group")
+            info = checkGroup(data)
         }
+        
     }
 }
 //get mouse location
 function getMouse(ev){
     x = ev.clientX
     y = ev.clientY
+    scx = window.scrollX
+    scy = window.scrollY
+    b = {}
+    b.x = x + scx + 10
+    b.y = y + scy + 10
     console.log("x = "+x)
     console.log("y = "+y)
+    console.log("scx = "+scx)
+    console.log("scy = "+scy)
+    return b
 }
 //check ip info from ip138
-function checkIp(ip){
+function checkIp(ip,place){
+    retinfo = ""
     //checkIp Info
     $.get("http://www.ip138.com/ips1388.asp?ip="+ip+"&action=2",function(data){
-        info = $(data)[27].getElementsByTagName("td")[2].getElementsByTagName("li")[0]["innerText"]
-        console.log(info)
+        retinfo = $(data)[27].getElementsByTagName("td")[2].getElementsByTagName("li")[0]["innerText"]
+        toggleInfoDiv(retinfo,place.x,place.y)
     })
+    console.log(retinfo)
+    return retinfo
 }
+
 //check device info from sys
 function checkDevice(device){
-    alert(data)
+    return device
+}
+//check group info
+function checkGroup(group){
+    return group
+}
+//add div to html
+function toggleInfoDiv(info,x,y){
+    $("#actinfo").append("<span>"+info+"</span>")
+    document.getElementById("infoDiv").style.top  = y+"px"
+    document.getElementById("infoDiv").style.left = x+"px"
+}
+// remove chindren nodes
+function removeChildNode(){
+
 }
