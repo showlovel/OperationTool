6ec832fe3e7b8bcb703d132de5efd8c2024ee91f
shengyu.yang <showlovel8@gmail.com>
2013-11-15 16:47:27 +0800

    add group check

--- app/scripts/process.js
+++ app/scripts/process.js
@@ -4,20 +4,20 @@ document.onmouseup=function(env){
     data = text.toString()
     if(data.length!=0 && data.length!=""){
         console.log(text.toString())
-        place = getMouse(env)
+        place    = getMouse(env)
         //check ip
         var info = ""
         if(/^\d+\.\d+\.\d+\.\d+$/.test(data)){
             console.log("is a ip")
-            checkIp(data,place)
+            checkIp(data)
         }else if(/^[A-Za-z]{3}-[A-Za-z]{2,3}-[A-Za-z0-9]{1}-[A-Za-z0-9]{3}$/.test(data)){
         //check device
             console.log("is a device")
-            checkDevice(data,place)
-        }else if(/^[A-Za-z0-9.-]+$/.test(data)){
+            checkDevice(data)
+        }else{
         //check group
             console.log("is group")
-            info = checkGroup(data)
+            checkGroup(data)
         }
         
     }
@@ -39,19 +39,19 @@ function getSelectText(){
 //get mouse location
 function getMouse(ev){
     var excursion = 15
-    b = {}
-    b.x = ev.clientX + window.scrollX + excursion
-    b.y = ev.clientY + window.scrollY + excursion
+    b             = {}
+    b.x           = ev.clientX + window.scrollX + excursion
+    b.y           = ev.clientY + window.scrollY + excursion
     return b
 }
 //check ip info from ip138
-function checkIp(ip,place){
+function checkIp(ip){
     retinfo = ""
     //checkIp Info
     $.get("http://www.ip138.com/ips1388.asp?ip="+ip+"&action=2",function(data){
-        retinfo = $(data)[27].getElementsByTagName("td")[2].getElementsByTagName("li")[0]["innerText"]
-        info = {}
-        info.title = ip
+        retinfo      = $(data)[27].getElementsByTagName("td")[2].getElementsByTagName("li")[0]["innerText"]
+        info         = {}
+        info.title   = ip
         info.content = "<tr><td>IP138:</td><td>"+retinfo.substr(6,ip.length)+"</td></tr>"
         toggleInfoDiv(info,place.x,place.y)
     })
@@ -62,12 +62,12 @@ function checkIp(ip,place){
 function checkDevice(device){
     $.get("http://localhost:3000/info/device.json?name="+device,function(data){
         console.log(data)
-        r1 = "<tr>"+"<td>备注:</td>"+"<td>"+data.description+"</td></tr>"
-        r2 = "<tr>"+"<td>Domain:</td>"+"<td>"+data.domain+"</td></tr>"
-        r3 = "<tr>"+"<td>设备状态:</td>"+"<td>"+data.devStatus+"</td></tr>"
-        content = r1+r2+r3
-        info = {}
-        info.title = device
+        r1           = "<tr>"+"<td>备注:</td>"+"<td>"+data.description+"</td></tr>"
+        r2           = "<tr>"+"<td>Domain:</td>"+"<td>"+data.domain+"</td></tr>"
+        r3           = "<tr>"+"<td>设备状态:</td>"+"<td>"+data.devStatus+"</td></tr>"
+        content      = r1+r2+r3
+        info         = {}
+        info.title   = device
         info.content = content
         toggleInfoDiv(info,place.x,place.y)
     })
@@ -75,6 +75,18 @@ function checkDevice(device){
 
 //check group info
 function checkGroup(group){
+    $.get("http://localhost:3000/info/group.json?name=" + group, function(data){
+        console.log(data)
+        r0           = "<tr>" + "<td>Total:</td>"   +  "<td>" + data.Total   + "</td></tr>"
+        r1           = "<tr>" + "<td>OPEN:</td>"    +  "<td>" + data.OPEN    + "</td></tr>"
+        r2           = "<tr>" + "<td>CLOSE:</td>"   +  "<td>" + data.CLOSE   + "</td></tr>"
+        r3           = "<tr>" + "<td>SUSPEND:</td>" +  "<td>" + data.SUSPEND + "</td></tr>"
+        content      = r0+r1+r2+r 3
+        info         = { }
+        info.title   = grou p
+        info.content = content
+        toggleInfoDiv(info,place.x,place.y)
+    })
     return group
 }
 //add div to html
@@ -83,11 +95,11 @@ function toggleInfoDiv(info,x,y){
     dislpayInfoDiv()
     removeInformationDiv()
 
-    $(".r").append("<span id=\"title\">"+info.title+"</span>")
-    $("#actinfo").append("<table id=\"content\">"+info.content+"</table>")
-    document.getElementById("infoDiv").style.top  = y+"px"
-    document.getElementById("infoDiv").style.left = x+"px"
-    setTimeout("undisplayInfoDiv()",2000)
+    $(".r").append("<span id=\"title\">" + info.title + "</span>")
+    $("#actinfo").append("<table id=\"content\">" + info.content + "</table>")
+    document.getElementById("infoDiv").style.top  = y + "px"
+    document.getElementById("infoDiv").style.left = x + "px"
+    setTimeout("undisplayInfoDiv()",3000)
 }
 // remove chindren nodes
 function removeInformationDiv(){
